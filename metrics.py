e0b64cef93e64dff1a61fdb2ed645cc166c56b5b
JasonTam <JasonTam22@gmail.com>
2015-08-14 13:50:43 -0400

    this repo is so disorganized lol

---
+++ metrics.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sklearn.metrics import make_scorer
+
+def gini(y_true, y_pred):
+    """
+    via: https://www.kaggle.com/jpopham91/liberty-mutual-group-property-inspection-prediction/gini-scoring-simple-and-efficient 
+        by jpopham91
+    Simple implementation of the (normalized) gini score in numpy
+    Fully vectorized, no python loops, zips, etc.
+    Significantly (>30x) faster than previous implementions
+    """
+    # check and get number of samples
+    assert y_true.shape == y_pred.shape
+    n_samples = y_true.shape[0]
+    
+    # sort rows on prediction column 
+    # (from largest to smallest)
+    arr = np.array([y_true, y_pred]).transpose()
+    true_order = arr[arr[:,0].argsort()][::-1,0]
+    pred_order = arr[arr[:,1].argsort()][::-1,0]
+    
+    # get Lorenz curves
+    L_true = np.cumsum(true_order) / np.sum(true_order)
+    L_pred = np.cumsum(pred_order) / np.sum(pred_order)
+    L_ones = np.linspace(0, 1, n_samples)
+    
+    # get Gini coefficients (area between curves)
+    G_true = np.sum(L_ones - L_true)
+    G_pred = np.sum(L_ones - L_pred)
+    
+    # normalize to true Gini coefficient
+    return G_pred/G_true
+
+def gini2(solution, submission):
+    df = zip(solution, submission, range(len(solution)))
+    df = sorted(df, key=lambda x: (x[1],-x[2]), reverse=True)
+    rand = [float(i+1)/float(len(df)) for i in range(len(df))]
+    totalPos = float(sum([x[0] for x in df]))
+    cumPosFound = [df[0][0]]
+    for i in range(1,len(df)):
+        cumPosFound.append(cumPosFound[len(cumPosFound)-1] + df[i][0])
+    Lorentz = [float(x)/totalPos for x in cumPosFound]
+    Gini = [Lorentz[i]-rand[i] for i in range(len(df))]
+    return sum(Gini)
+
+def normalized_gini(solution, submission):
+    normalized_gini = gini2(solution, submission)/gini2(solution, solution)
+    return normalized_gini
+
+gini_score = make_scorer(gini, greater_is_better=True)
+gini_loss = make_scorer(gini, greater_is_better=False)
+
+
